2066266535f5930deb11be5d14f839b1ef9f472e
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-03-17 12:13:53 +0530

    Page: LoginPage

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="center">
+    <v-row>
+      <v-col cols = "10" sm="6" offset-sm="3" md="4" offset-md="4" offset="1">
+        <v-avatar size="95" tile>
+          <v-img src="@/assets/logo.png"></v-img>
+        </v-avatar>
+        <v-text-field label="Email"> </v-text-field>
+        <v-text-field type="password" label="Password"> </v-text-field>
+        <v-btn block @click="onTap"> Login </v-btn>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onTap() {
+      alert("clicked");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
