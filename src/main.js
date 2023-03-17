092317c970717535ed8254540b5b741a06722978
swastikagupta862@gmail.com <swastikagupta862@gmail.com>
2023-03-17 10:55:30 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import GlobalMixins from '@/utils/GlobalMixins.vue'
+import AppConstants from '@/utils/AppConstants'
+
+Vue.mixin(GlobalMixins)
+Vue.prototype.$const = AppConstants
 
 Vue.config.productionTip = false
 
